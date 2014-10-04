176b713c31f1bdf1fb77d4999c952a285ce477f6
chao <zhangxc1120@qq.com>
2014-10-04 23:53:49 +0800

    初始化项目

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by chao on 14-10-4.
+//  Copyright (c) 2014年 zhangxc1120. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by chao on 14-10-4.
+//  Copyright (c) 2014年 zhangxc1120. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
